d81e7848c00bf4403a713b64b22a473986794031
Misty <gyc990326@gmail.com>
2022-11-10 00:37:29 +0800

    Change to gosetuidgid

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main // import "github.com/tianon/gosu"
+package main // import "github.com/tianon/gosetuidgid"
 
 import (
 	"bytes"
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 	"syscall"
 	"text/template"
@@ -25,13 +26,12 @@ func version() string {
 
 func usage() string {
 	t := template.Must(template.New("usage").Parse(`
-Usage: {{ .Self }} user-spec command [args]
-   eg: {{ .Self }} tianon bash
-       {{ .Self }} nobody:root bash -c 'whoami && id'
-       {{ .Self }} 1000:1 id
+Usage: {{ .Self }} uid gids command [args]
+   eg: {{ .Self }} 1000 1000 sh
+       {{ .Self }} 1000 1000,1001,1002 sh
 
 {{ .Self }} version: {{ .Version }}
-{{ .Self }} license: Apache-2.0 (full text at https://github.com/tianon/gosu)
+{{ .Self }} license: Apache-2.0 (full text at https://github.com/tianon/gosetuidgid)
 `))
 	var b bytes.Buffer
 	template.Must(t, t.Execute(&b, struct {
@@ -47,43 +47,47 @@ Usage: {{ .Self }} user-spec command [args]
 func main() {
 	log.SetFlags(0) // no timestamps on our logs
 
-	if ok := os.Getenv("GOSU_PLEASE_LET_ME_BE_COMPLETELY_INSECURE_I_GET_TO_KEEP_ALL_THE_PIECES"); ok != "I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched C-beams glitter in the dark near the Tannhäuser Gate. All those moments will be lost in time, like tears in rain. Time to die." {
-		if fi, err := os.Stat("/proc/self/exe"); err != nil {
-			log.Fatalf("error: %v", err)
-		} else if fi.Mode()&os.ModeSetuid != 0 {
-			// ... oh no
-			log.Fatalf("error: %q appears to be installed with the 'setuid' bit set, which is an *extremely* insecure and completely unsupported configuration! (what you want instead is likely 'sudo' or 'su')", os.Args[0])
-		}
-	}
-
-	if len(os.Args) >= 2 {
-		switch os.Args[1] {
-		case "--help", "-h", "-?":
-			fmt.Println(usage())
-			os.Exit(0)
-		case "--version", "-v":
-			fmt.Println(version())
-			os.Exit(0)
-		}
-	}
-	if len(os.Args) <= 2 {
+	if len(os.Args) <= 3 {
 		log.Println(usage())
 		os.Exit(1)
 	}
 
-	// clear HOME so that SetupUser will set it
-	os.Unsetenv("HOME")
+	uidStr := os.Args[1]
+	uid, err := strconv.Atoi(uidStr)
+	if err != nil {
+		fmt.Println(usage())
+		os.Exit(1)
+	}
+	gidStr := os.Args[2]
+	commands := os.Args[3:]
 
-	if err := SetupUser(os.Args[1]); err != nil {
-		log.Fatalf("error: failed switching to %q: %v", os.Args[1], err)
+	gidsStrList := strings.Split(gidStr, ",")
+	gids := make([]int, 0)
+	for _, gid := range gidsStrList {
+		gidNum, err := strconv.Atoi(gid)
+		if err != nil {
+			fmt.Printf("Unknown gid: %s\n", gidNum)
+			os.Exit(1)
+		}
+		gids = append(gids, gidNum)
 	}
 
-	name, err := exec.LookPath(os.Args[2])
+	if err := syscall.Setgroups(gids); err != nil {
+		fmt.Printf("Setgroups failed: %v\n", err)
+	}
+	if err := syscall.Setgid(gids[0]); err != nil {
+		fmt.Printf("Setgid failed: %v\n", err)
+	}
+	if err := syscall.Setuid(uid); err != nil {
+		fmt.Printf("Setuid failed: %v\n", err)
+	}
+
+	name, err := exec.LookPath(commands[0])
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
 
-	if err = syscall.Exec(name, os.Args[2:], os.Environ()); err != nil {
+	if err = syscall.Exec(name, commands, os.Environ()); err != nil {
 		log.Fatalf("error: exec failed: %v", err)
 	}
 }
